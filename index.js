78ecaf4dea33ba179099544a6129cd92404bac46
Mehedi Hasan <mehedi@99robots.com>
2025-12-21 16:13:25 +0600

    Vercel Deployed

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://scholar-stream-client-three.vercel.app",
+    ],
     credentials: true,
   })
 );
